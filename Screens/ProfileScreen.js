660a88ad94c7b1bb92f4c2e0371de706fa5d90d2
Alina Varkki <alina.varkki.2018@uni.strath.ac.uk>
2021-04-17 20:38:47 +0300

    edited profile screen

--- Screens/ProfileScreen.js
+++ Screens/ProfileScreen.js
@@ -3,6 +3,7 @@ import {SafeAreaView, StyleSheet, TouchableOpacity, View, Image, ImageBackground
 import {useNavigation} from '@react-navigation/native';
 import ColourPalette from "../ColourPalette";
 import Icon from "react-native-vector-icons/Octicons";
+import Icon1 from "react-native-vector-icons/FontAwesome5";
 
 const ProfileScreen = () => {
 
@@ -24,22 +25,47 @@ const ProfileScreen = () => {
 
             <Image style = {{alignSelf: "center", height: 150, width:150, marginBottom: 50}} source={require('../Images/profilePhoto.jpg')}/>
 
-            <View style = {{backgroundColor: '#f4f5fa', borderRadius: 15, height: 70, justifyContent:'center', paddingLeft: 15, margin:10}}>
+            <View style = {{flexDirection: 'row', alignItems: "center", justifyContent: "space-between", backgroundColor: '#f4f5fa', borderRadius: 15, height: 70, paddingLeft: 15, margin:10}}>
+                <View>
                 <Text >Name</Text>
-                <Text style={{fontWeight: "bold"}}>Slavka Borovska</Text>
+                    <Text style={{alignSelf: "flex-start", fontWeight: "bold"}}>Slavka Borovska</Text>
+                </View>
+                <View>
+                    <Icon1 style={{alignSelf: "flex-end", marginRight: 15}} name='pen' size={25} color= {ColourPalette.purple} />
+                </View>
             </View>
-            <View style = {{backgroundColor: '#f4f5fa', borderRadius: 15, height: 70, justifyContent:'center', paddingLeft: 15, margin:10}}>
+            <View style = {{flexDirection: 'row', alignItems: "center", justifyContent: "space-between", backgroundColor: '#f4f5fa', borderRadius: 15, height: 70, paddingLeft: 15, margin:10}}>
+                <View>
                 <Text >Email</Text>
-                <Text style={{fontWeight: "bold"}}>slavkaborovska@gmail.com</Text>
+                    <Text style={{alignSelf: "flex-start", fontWeight: "bold"}}>slavkaborovska@gmail.com</Text>
+                </View>
+                <View>
+                    <Icon1 style={{alignSelf: "flex-end", marginRight: 15}} name='pen' size={25} color= {ColourPalette.purple} />
+                </View>
             </View>
-            <View style = {{backgroundColor: '#f4f5fa', borderRadius: 15, height: 70, justifyContent:'center', paddingLeft: 15, margin:10}}>
-                <Text >Phone</Text>
-                <Text style={{fontWeight: "bold"}}>+44 8484 567838</Text>
+            <View style = {{flexDirection: 'row', alignItems: "center", justifyContent: "space-between", backgroundColor: '#f4f5fa', borderRadius: 15, height: 80, paddingLeft: 15, margin:10}}>
+                <View>
+                    <Text >Phone</Text>
+                    <Text style={{alignSelf: "flex-start", fontWeight: "bold"}}>+44 8484 567838</Text>
+                    <View style={{flexDirection: 'row',backgroundColor: "#a8fadf", height: 23,width: 79, justifyContent: "center", borderRadius:10}}>
+                        <Text style={{alignSelf: "flex-start"}}>✓ verified</Text>
+                    </View>
+                </View>
+                <View>
+                    <Icon1 style={{alignSelf: "flex-end", marginRight: 15}} name='pen' size={25} color= {ColourPalette.purple} />
+                </View>
             </View>
-            <View style = {{backgroundColor: '#f4f5fa', borderRadius: 15, height: 70, justifyContent:'center', paddingLeft: 15, margin:10}}>
+
+            <View style = {{flexDirection: 'row', alignItems: "center", justifyContent: "space-between", backgroundColor: '#f4f5fa', borderRadius: 15, height: 70, paddingLeft: 15, margin:10}}>
+                <View>
                 <Text >Address</Text>
-                <Text style={{fontWeight: "bold"}}>G1 1QL, 1-4 Personage row</Text>
+                    <Text style={{alignSelf: "flex-start", fontWeight: "bold"}}>G1 1QL, 1-4 Personage row</Text>
+                </View>
+                <View>
+                    <Icon1 style={{alignSelf: "flex-end", marginRight: 15}} name='pen' size={25} color= {ColourPalette.purple} />
+                </View>
             </View>
+
         </SafeAreaView>
 
     );
